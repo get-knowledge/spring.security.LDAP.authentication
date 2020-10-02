c2532399e6ef24cfa8c3a107d27f81b3b73545d0
91981 <91981@LAPTOP-N6A28HHE>
2020-10-02 22:42:52 +0530

    First Commit For LDAP With Spring Boot

---
+++ src/main/java/com/beyond/spring/security/LDAP/authentication/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.beyond.spring.security.LDAP.authentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Welcome to the Spring Security LDAP Authentication Guide!";
+	}
+}
